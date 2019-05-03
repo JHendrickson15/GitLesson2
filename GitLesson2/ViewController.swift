23cbd0be46591fd71fff6950e2baa6b30a2db742
Jordan Hendrickson <jordanhendrickson@Jordans-MacBook-Pro.local>
2019-05-03 16:00:57 -0600

    Initial Commit

---
+++ GitLesson2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitLesson2
+//
+//  Created by Jordan Hendrickson on 5/3/19.
+//  Copyright © 2019 Jordan Hendrickson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
